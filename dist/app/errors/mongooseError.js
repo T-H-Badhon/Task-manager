3162b661ca0281045602f69de73bb4186c0eea8d
Md. Tanvir Hasan <136628651+T-H-Badhon@users.noreply.github.com>
2025-01-27 00:30:44 +0600

    Delete dist directory

--- dist/app/errors/mongooseError.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MongooseErrorMessageGenerator = void 0;
-const MongooseErrorMessageGenerator = (err) => {
-    return err.message;
-};
-exports.MongooseErrorMessageGenerator = MongooseErrorMessageGenerator;
